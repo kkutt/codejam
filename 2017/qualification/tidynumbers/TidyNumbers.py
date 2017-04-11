51e50fd44f1ce9085911aa7c6752ccfd52606fe3
Krzysztof Kutt <krzysztof.kutt@gmail.com>
2017-04-11 20:45:27 +0200

    Coding utf-8 is obsolete in python3. Overview changed to block comment

--- 2017/qualification/tidynumbers/TidyNumbers.py
+++ 2017/qualification/tidynumbers/TidyNumbers.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" Google Code Jam 2017. Qualification Round
-    Problem B. Tidy Numbers
-    
-    * Problem
-    Tatiana likes to keep things tidy. Her toys are sorted from smallest to
-    largest, her pencils are sorted from shortest to longest and her computers
-    from oldest to newest.
-    One day, when practicing her counting skills, she noticed that some
-    integers, when written in base 10 with no leading zeroes, have their digits
-    sorted in non-decreasing order. Some examples of this are 8, 123, 555, and
-    224488. She decided to call these numbers tidy. Numbers that do not have
-    this property, like 20, 321, 495 and 999990, are not tidy.
-    She just finished counting all positive integers in ascending order from
-    1 to N. What was the last tidy number she counted?
 
-    * Input
-    The first line of the input gives the number of test cases, T. T lines
-    follow. Each line describes a test case with a single integer N, the last
-    number counted by Tatiana.
-    * Output
-    For each test case, output one line containing Case #x: y, where x is the
-    test case number (starting from 1) and y is the last tidy number counted by
-    Tatiana.
-    
-    * Limits
-    1 ≤ T ≤ 100.
-    Small dataset: 1 ≤ N ≤ 1000.
-    Large dataset: 1 ≤ N ≤ 1018.
-
-    * Sample
-    Input               Output 
-    4
-    132                 Case #1: 129
-    1000                Case #2: 999
-    7                   Case #3: 7
-    111111111111111110  Case #4: 99999999999999999
-    Note that the last sample case would not appear in the Small dataset.
-    """
+# Google Code Jam 2017. Qualification Round
+# Problem B. Tidy Numbers
+#
+# * Problem
+# Tatiana likes to keep things tidy. Her toys are sorted from smallest to
+# largest, her pencils are sorted from shortest to longest and her computers
+# from oldest to newest.
+# One day, when practicing her counting skills, she noticed that some
+# integers, when written in base 10 with no leading zeroes, have their digits
+# sorted in non-decreasing order. Some examples of this are 8, 123, 555, and
+# 224488. She decided to call these numbers tidy. Numbers that do not have
+# this property, like 20, 321, 495 and 999990, are not tidy.
+# She just finished counting all positive integers in ascending order from
+# 1 to N. What was the last tidy number she counted?
+#
+# * Input
+# The first line of the input gives the number of test cases, T. T lines
+# follow. Each line describes a test case with a single integer N, the last
+# number counted by Tatiana.
+# * Output
+# For each test case, output one line containing Case #x: y, where x is the
+# test case number (starting from 1) and y is the last tidy number counted by
+# Tatiana.
+#
+# * Limits
+# 1 ≤ T ≤ 100.
+# Small dataset: 1 ≤ N ≤ 1000.
+# Large dataset: 1 ≤ N ≤ 1018.
+#
+# * Sample
+# Input               Output
+# 4
+# 132                 Case #1: 129
+# 1000                Case #2: 999
+# 7                   Case #3: 7
+# 111111111111111110  Case #4: 99999999999999999
+# Note that the last sample case would not appear in the Small dataset.
 
 __author__ = "Krzysztof Kutt"
 __copyright__ = "Copyright 2017, Krzysztof Kutt"
